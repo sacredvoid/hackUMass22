7790ada59caca999da76bcf57322c4a2ca259bee
Rajat Keshri <keshri.r@northeastern.edu>
2022-11-12 10:43:56 -0500

    new updates

--- app/MetaIdentity.py
+++ app/MetaIdentity.py
@@ -1,26 +1,34 @@
 import speech_recognition as sr
 from gtts import gTTS
+#import pyttsx3
 import os
 import requests
 from Secrets import API_TOKEN
 from PIL import Image
+#import moviepy.editor as mp
 
 GENERATED_ASSETS = './assets/generated'
 UPLOADED_ASSETS = './assets/uploaded'
+#engine = pyttsx3.init()
 
 def audiototext(filename):
     r = sr.Recognizer()
     with sr.AudioFile(filename) as source:
-        # listen for the data (load audio to memory)
         audio_data = r.record(source)
-        # recognize (convert from speech to text)
         text = r.recognize_google(audio_data)
         return text
 
 def texttoaudio(text):
-    language = 'en'
-    myobj = gTTS(text=reply, lang=language, slow=False)
-    myobj.save(GENERATED_ASSETS+"reply.mp3")
+    if text!="":
+        #language = 'en'
+        #myobj = gTTS(text=text, lang=language, slow=False, tld='com.in')
+        #myobj.save(GENERATED_ASSETS+"/reply.mp3")
+
+        # male voice
+        engine.save_to_file(text, GENERATED_ASSETS+"/reply.mp3")
+        engine.runAndWait()
+
+        # read audio and apply style transfer
 
 def chatbot(textinput,  past_user_inputs, generated_responses):
 
@@ -42,10 +50,7 @@ def chatbot(textinput,  past_user_inputs, generated_responses):
 
 
 #--------------------------------
-def s2t2huggingface(past_user_inputs, max_convo, generated_responses):
-    filename = UPLOADED_ASSETS+"input_audio.wav"
-    text = audiototext(filename)
-
+def s2t2huggingface(past_user_inputs, max_convo, generated_responses, text):
     # Storing user input
     if len(past_user_inputs) == max_convo:
         past_user_inputs.pop(0)
@@ -54,12 +59,10 @@ def s2t2huggingface(past_user_inputs, max_convo, generated_responses):
     # send text to NLP GPT3
     # get the reply back from NLP
     reply = chatbot(text, past_user_inputs, generated_responses)
-
     # Storing chat output
     if len(generated_responses) == max_convo:
         generated_responses.pop(0)
     generated_responses.append(reply)
-    print(reply)
 
     return reply
 
@@ -67,11 +70,24 @@ def s2t2huggingface(past_user_inputs, max_convo, generated_responses):
 # get the image from user
 # im = Image.open(UPLOADED_ASSETS+"input_img.jpg") 
   
-
 # Get audio from user
 
-
-
 # convert to toon - https://github.com/williamyang1991/VToonify
+def image2toon(image):
+    pass
 
 # toon + audio to video synthezier (Takes input image) - https://github.com/tg-bomze/Face-Image-Motion-Model
+def audiotovideo(text):
+    if text!="":
+        #open audio
+        audio = mp.AudioFileClip(GENERATED_ASSETS + "/reply.wav")
+        #open video
+        video1 = mp.VideoFileClip("./assets/video1.mp4")
+        #merge 
+        final = video1.set_audio(audio)
+        final.write_videofile("output.mp4",codec= 'mpeg4' ,audio_codec='libvorbis')
+
+
+def imageonvideo(image,video):
+    pass
+
--- app/app.py
+++ app/app.py
@@ -1,31 +1,63 @@
 import os
 import streamlit as st
 import streamlit.components.v1 as components
-from MetaIdentity import s2t2huggingface, GENERATED_ASSETS, UPLOADED_ASSETS
+from MetaIdentity import s2t2huggingface, GENERATED_ASSETS, UPLOADED_ASSETS, texttoaudio, audiotovideo, audiototext
+
+import streamlit as st
+from audiorecorder import audiorecorder
+
+
 # def file_writer(file_like_object):
 PAST_USER_INPUTS = []
 MAX_CONVO_WINDOW = 5
 GENERATED_RESPONSE = []
-
+DURATION = 5
 
 def write_audio_file(path, data):
-    with open(path, mode='bx') as f:
-        f.write(data)
+    data.export(path, format="wav")
+    #with open(path, mode='bx') as f:
+    #    f.write(data)
+
+
 
 def audio_app(st_audiorec):
     st.write("""
     # RIP
     Our first app
     """)
-    st_audiorec()
+
+    audio = audiorecorder("Click to record", "Recording...")
+    path = os.path.join(UPLOADED_ASSETS,"/input_audio.wav")
+    
+    if len(audio) > 0:
+        # To play audio in frontend:
+        st.audio(audio)
+        
+        wav_file = open(path, "wb")
+        wav_file.write(audio.tobytes())
+
+        text = audiototext(path)
+        botreply = s2t2huggingface(PAST_USER_INPUTS, MAX_CONVO_WINDOW, GENERATED_RESPONSE, text)
+        texttoaudio(botreply)
+        audiotovideo(botreply)
+        print("In app.py, response from bot:"+botreply)
+    
+    #st_audiorec()
     uploaded_file = st.file_uploader("Choose a file")
     if(uploaded_file is not None):
         # Call the s2t2huggingface here
-        path = os.path.join(UPLOADED_ASSETS,"input_audio.wav")
         write_audio_file(path,uploaded_file)
-        botreply = s2t2huggingface(PAST_USER_INPUTS, MAX_CONVO_WINDOW, GENERATED_RESPONSE)
+
+        text = audiototext(path)
+        botreply = s2t2huggingface(PAST_USER_INPUTS, MAX_CONVO_WINDOW, GENERATED_RESPONSE, text)
+        texttoaudio(botreply)
+        audiotovideo(botreply)
         print("In app.py, response from bot:"+botreply)
 
+
+
+    
+    
 
 def main():
     parent_dir = os.path.dirname(os.path.abspath(__file__))
