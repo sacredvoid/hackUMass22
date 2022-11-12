6d4823e8025984e7dd297d140bc3aeefccee093e
Samanvya Tripathi <samanu09@gmail.com>
2022-11-12 12:52:54 -0500

    haha

--- app/app.py
+++ app/app.py
@@ -2,10 +2,11 @@ import os
 import streamlit as st
 import streamlit.components.v1 as components
 from MetaIdentity import s2t2huggingface, GENERATED_ASSETS, UPLOADED_ASSETS, texttoaudio, audiotovideo, audiototext
-
+import time
 import streamlit as st
 from audiorecorder import audiorecorder
-
+from sr_audio_recorder import record_audio, save_audio
+PARENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # def file_writer(file_like_object):
 PAST_USER_INPUTS = []
@@ -13,34 +14,27 @@ MAX_CONVO_WINDOW = 5
 GENERATED_RESPONSE = []
 DURATION = 5
 
-def write_audio_file(path, data):
-    data.export(path, format="wav")
-    #with open(path, mode='bx') as f:
-    #    f.write(data)
 
-
-
-def audio_app(st_audiorec):
+def audio_app():
     st.write("""
     # RIP
     Our first app
     """)
+    # Call button to call sr.listen()
+    # audio = audiorecorder("Click to record", "Recording...")
+    path = os.path.join(PARENT_DIR,"assets/uploaded/sr_input_audio.wav")
+    # path = "input2_audio.mp3"
+    st.write("""
+    Trying to run this file
+    """)
+    save_audio(path,record_audio()) 
 
-    audio = audiorecorder("Click to record", "Recording...")
-    path = os.path.join(UPLOADED_ASSETS,"/input_audio.wav")
     
-    if len(audio) > 0:
-        # To play audio in frontend:
-        st.audio(audio)
-        
-        wav_file = open(path, "wb")
-        wav_file.write(audio.tobytes())
-
-        text = audiototext(path)
-        botreply = s2t2huggingface(PAST_USER_INPUTS, MAX_CONVO_WINDOW, GENERATED_RESPONSE, text)
-        texttoaudio(botreply)
-        audiotovideo(botreply)
-        print("In app.py, response from bot:"+botreply)
+    text = audiototext(path)
+    botreply = s2t2huggingface(PAST_USER_INPUTS, MAX_CONVO_WINDOW, GENERATED_RESPONSE, text)
+    texttoaudio(botreply)
+    audiotovideo(botreply)
+    print("In app.py, response from bot:"+botreply)
     
     #st_audiorec()
     uploaded_file = st.file_uploader("Choose a file")
@@ -60,10 +54,9 @@ def audio_app(st_audiorec):
     
 
 def main():
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "st_audiorec/frontend/build")
-    st_audiorec = components.declare_component("st_audiorec", path=build_dir)
-    audio_app(st_audiorec)
+    # build_dir = os.path.join(PARENT_DIR, "st_audiorec/frontend/build")
+    # st_audiorec = components.declare_component("st_audiorec", path=build_dir)
+    audio_app()
 
 
 if __name__ == "__main__":
